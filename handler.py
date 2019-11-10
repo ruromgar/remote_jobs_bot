4302c9d115141f083009d56621d7c43df98999a9
Rubén <31738826+ruromgar@users.noreply.github.com>
2019-11-10 20:07:37 +0100

    Update handler.py

--- handler.py
+++ handler.py
@@ -2,9 +2,6 @@ import telegram
 import sys
 import os
 
-main_folder = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(main_folder, "./libraries"))
-
 TOKEN = os.environ['TELEGRAM_TOKEN']
 CHAT_ID = 000000  # Change this
 
